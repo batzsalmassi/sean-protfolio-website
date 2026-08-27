adf274f7c128a858a129cb5ec330de284fe2fd47
batzsalmassi <sean.salmassi1@gmail.com>
2026-08-27 21:21:37 +0300

    Fix Bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,7 +77,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["DevSecOps Engineer", "DevOps Engineer", "Cloud Security", "Systems Engineer"],
+    strings: ["DevSecOps Engineer", "DevOps Engineer", "Cloud Security"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
